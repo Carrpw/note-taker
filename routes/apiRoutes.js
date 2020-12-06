33e35ddc504ddd86a711b19607421984e610f83d
Paul Carr <pwcarr15@gmail.com>
2020-12-05 21:20:47 -0500

    changes to routes.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,52 +1,36 @@
 const fs = require("fs");
-var data = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
-
+let data = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
 
 module.exports = function(app) {
-
     app.get("/api/notes", function(req, res) {
-       
         res.json(data);
-
     });
-
-    app.get("/api/notes/:id", function(req, res) {
-
+    app.get("/api/notes:id", function(req, res) {
         res.json(data[Number(req.params.id)]);
-
     });
-
-
     app.post("/api/notes", function(req, res) {
-
         let newNote = req.body;
         let uniqueId = (data.length).toString();
         console.log(uniqueId);
         newNote.id = uniqueId;
         data.push(newNote);
-        
         fs.writeFileSync("./db/db.json", JSON.stringify(data), function(err) {
-            if (err) throw (err);        
-        }); 
-
-        res.json(data);    
-
+            if (err) throw (err);
+        });
+        res.json(data);
     });
-
-    
-    app.delete("/api/notes/:id", function(req, res) {
-
-        let noteId = req.params.id;
+    app.delete("/api/notes/:id", function(req,res) {
+        let noteID = req.params.id;
         let newId = 0;
-        console.log(`Deleting note with id ${noteId}`);
+        console.log(`Deleting note with id ${noteID}`);
         data = data.filter(currentNote => {
-           return currentNote.id != noteId;
+            return currentNote.id !=noteID;
         });
         for (currentNote of data) {
             currentNote.id = newId.toString();
             newId++;
-        }
+        };
         fs.writeFileSync("./db/db.json", JSON.stringify(data));
         res.json(data);
-    })}; 
-
+    });
+};
