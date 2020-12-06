d37fbbf8ab4808e47f532e102941d6730f607ff4
Paul Carr <pwcarr15@gmail.com>
2020-12-05 19:38:54 -0500

    Reorganized.

--- server.js
+++ server.js
@@ -1,68 +1,21 @@
-// Allows express to work with the front, path for filename paths, fs to read & write files
+
 const express = require("express");
-const fs = require("fs");
 const path = require("path");
 
-
-// Sets up express and the where the port is
+// Create server application at port 3000
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-
-// Empty array for user notes
-let notes = [];
-
-
-// Middleware
-app.use(express.json());
+// Read URL or JSON
 app.use(express.urlencoded({extended: true}));
-app.use(express.static(path.join(__dirname)));
+app.use(express.json());
+app.use(express.static("public"));
 
+// Include js files
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
 
-// 
-app.get("/api/notes", function(req, res) {
-    notes = fs.readFileSync("db.json", "utf8");
-    notes = JSON.parse(notes);
-    res.json(notes);
-});
-
-app.post("/api/notes", function(req, res) {
-    notes = fs.readFileSync("db.json", "utf8");
-    notes = JSON.parse(notes);
-    req.body.id = notes.length;
-    notes.push(req.body);
-    notes = JSON.stringify(notes);
-    fs.writeFile("db.json", notes, "utf8");
-    res.json(JSON.parse(notes)); 
-});
-
-app.delete("/api/notes/:id", function(req, res) {
-    notes = fs.readFileSync("db.json", "utf8");
-    notes = JSON.parse(notes);
-    notes = notes.filter(function(note) {
-        return note.id != req.params.id;
-    });
-    notes = JSON.stringify(notes);
-    fs.writeFile("db.json", notes, "utf8");
-    res.send(JSON.parse(notes));
-});
-
-
-// Routes to transfer users with the home being defaulted to if an incorrect route it written
-app.get("/notes", function(req, res) {
-    res.sendFile(path.join(__dirname, "/notes.html"));
-});
-
-app.get("/api/notes", function(req, res) {
-    return res.sendFile(path.json(__dirname, "db.json"));
-});
-
-app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "/index.html"));
-});
-
-
-// Lets the server listen
+// Add listener
 app.listen(PORT, function() {
-    console.log("Server is listening on PORT " + PORT)
-})
+    console.log("App listening on PORT: " + PORT);
+});
